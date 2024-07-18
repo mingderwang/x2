92a2fda842eacc8758156086549d599f22d6ea7d
mingderwang <mingderwang@me.com>
2024-07-18 14:51:17 +0800

    rename to x2

--- src/pages/api/items.ts
+++ src/pages/api/items.ts
@@ -1,3 +1,14 @@
+import { createClient } from '@vercel/kv';
+
+const kv = createClient({
+  url: 'https://<hostname>.redis.vercel-storage.com',
+  token: '<token>',
+});
+
+await kv.set('key', 'value');
+let data = await kv.get('key');
+console.log(data); // 'value'
+
 
 export const GET = async ({  }) => {
   return new Response(JSON.stringify([

604ee25ca4d59d890d57757a57ee43fd81b211bb
mingderwang <mingderwang@me.com>
2024-07-18 17:05:01 +0800

    kv utils

--- src/pages/api/items.ts
+++ src/pages/api/items.ts
@@ -1,18 +1,7 @@
-import { createClient } from '@vercel/kv';
-
-const kv = createClient({
-  url: 'https://<hostname>.redis.vercel-storage.com',
-  token: '<token>',
-});
-
-await kv.set('key', 'value');
-let data = await kv.get('key');
-console.log(data); // 'value'
-
+import { getUser } from '../../utils/kv'
 
 export const GET = async ({  }) => {
   return new Response(JSON.stringify([
-    'Buy the milk', 
-    'Write a blog post'
+    await getUser() 
   ]))
 }
